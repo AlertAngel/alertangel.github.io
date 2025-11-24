adca7ec1a4eff7a5a6be5d35e15774e85143c906
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-11-24 11:16:20 +0530

    Added startupscreen

---
+++ src/startup.rs
@@ -0,0 +1,45 @@
+use ratatui::{
+    layout::{Alignment, Constraint, Layout},
+    style::{Color,Style, Stylize},
+    widgets::Paragraph,
+    Terminal,
+};
+
+const LOGO: &str = r#"
+ █████╗ ██╗     ███████╗██████╗ ████████╗
+██╔══██╗██║     ██╔════╝██╔══██╗╚══██╔══╝
+███████║██║     █████╗  ██████╔╝   ██║   
+██╔══██║██║     ██╔══╝  ██╔══██╗   ██║   
+██║  ██║███████╗███████╗██║  ██║   ██║   
+╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝   
+ █████╗ ███╗   ██╗ ██████╗ ███████╗██╗     
+██╔══██╗████╗  ██║██╔════╝ ██╔════╝██║     
+███████║██╔██╗ ██║██║  ███╗█████╗  ██║     
+██╔══██║██║╚██╗██║██║   ██║██╔══╝  ██║     
+██║  ██║██║ ╚████║╚██████╔╝███████╗███████╗
+╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚══════╝
+"#;
+
+pub struct StartupScreen;
+
+impl  StartupScreen {
+    pub fn start<B>(terminal: &mut Terminal<B>)
+    where
+        B: ratatui::backend::Backend,
+    {
+        terminal.draw(|frame| {
+            let layout = Layout::vertical([
+                Constraint::Percentage(20),
+                Constraint::Min(10),
+                Constraint::Percentage(20),
+            ])
+                .split(frame.area());
+
+            let logo_widget = Paragraph::new(LOGO)
+                .style(Style::default().fg(Color::LightYellow).bold())
+                .alignment(Alignment::Center);
+
+            frame.render_widget(logo_widget, layout[1]);
+        }).unwrap();
+    }
+}
